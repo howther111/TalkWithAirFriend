fa5c4b1afbdc4970466fe3a78d6cb6378625f53d
Elauno Mitsugi <atsukiexfeel@yahoo.co.jp>
2022-02-12 13:34:21 +0900

    修正
    修正

--- main.py
+++ main.py
@@ -20,6 +20,11 @@ while True:
         recogn = r.recognize_google(audio, language='ja-JP')
         print(recogn)
 
+        # "さようなら" と言ったら音声認識を止める
+        if r.recognize_google(audio, language='ja-JP') == "さようなら" :
+            print("end")
+            break
+
         pyperclip.copy(recogn)
 
         pyautogui.click(x=1050, y=680, interval=0.5, button="left")
@@ -37,10 +42,6 @@ while True:
         voice.Rate = 0  # [-10 to 10]
         voice.Speak(clip_str)
 
-        # "ストップ" と言ったら音声認識を止める
-        if r.recognize_google(audio, language='ja-JP') == "さようなら" :
-            print("end")
-            break
 
     # 以下は認識できなかったときに止まらないように。
     except sr.UnknownValueError:
